bbe6cf9fec5aa8d8edd545121cd32916eb4759d4
polina-shimolina <shimolina.polina@gmail.com>
2020-09-07 22:51:25 +0300

    1

---
+++ WpfApp1/WpfApp1/ClassSearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace WpfApp1
+{
+    class ClassSearch
+    {
+        public string result;
+        WebRequest request;
+        Weather weather;
+        public ClassSearch() { }
+        public void ChangeRequest(string input)
+        {
+            request = WebRequest.Create(input);
+        }
+        public void Search()
+        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            weather = JsonConvert.DeserializeObject<Weather>(responseFromServer);
+            result = $"temperature in {weather.name}: {weather.main.temp} °C";
+        }
+        public void MessageShow()
+        {
+            MessageBox.Show(result);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public string url = string.Empty;
+        ClassSearch weath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            weath = new ClassSearch();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            url += "http://api.openweathermap.org/data/2.5/weather?q=";
+            url += CityName.Text.ToString();
+            url += "&appid=ecc0ea77210f8032ef8804a80a9f40f0&units=metric";
+
+            weath.ChangeRequest(url); 
+            weath.Search();
+
+            weath.MessageShow();
+        }
+    }
+}
